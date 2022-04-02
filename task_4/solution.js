93c8508d55c3d9c4d8e6466d654d86cc5715c6bd
Anna Mt <anna@tech35.ru>
2022-04-02 22:05:37 +0300

    task_4_remake1

--- task_4/solution.js
+++ task_4/solution.js
@@ -5,15 +5,15 @@ function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products;
     
-    return parseProducts(products);
+    return products;
 }
 
 function renderProductsCards(json) {
     
     clearProducts();
-    products = parseProducts(json);
+    let products = parseProducts(json);
     let length = products.length;
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < length; i += 1) {
         addProduct(products[i]);
      }
 }
